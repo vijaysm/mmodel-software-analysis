4142062d355a058c71c8eee65ee052f8bd00c1ca
Sean Colby <seanmcolby@gmail.com>
2016-07-14 16:24:36 -0700

    enforced pep8 formatting

--- mmodel-software-survey.py
+++ mmodel-software-survey.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python
 
-import csv, subprocess
+import csv
+import subprocess
+import sys
+import os
 
 sandbox_dir = 'sandbox'
 subprocess.call(["mkdir", "-p", sandbox_dir])
 
+
 class MModelTool:
-    def __init__(self,Tool,langs,vcs,url):
+    def __init__(self, Tool, langs, vcs, url):
         self.Name = Tool
         self.Languages = langs
         self.VCS = vcs
         self.Repository = url
 
-    def __init__(self,ToolDict):
+    def __init__(self, ToolDict):
         self.Name = ToolDict['Name']
         self.Languages = ToolDict['Languages']
         self.VCS = ToolDict['VCS']
@@ -26,63 +30,68 @@ with open('data/mmodel-survey-tools.csv', 'rb') as csvfile:
     rowreader = csv.DictReader(csvfile)
     for row in rowreader:
         SurveyTools.append(row)
-    #print SurveyTools
+    # print SurveyTools
 
 # Replace with a recursive parse of the CSV file to get the tool list
 # tool = MModelTool('DTK','C++','Git','https://github.com/ORNL-CEES/DataTransferKit.git')
-#tool = MModelTool('DTK','C++','None','ftp://ftp.mcs.anl.gov/pub/fathom/moab-nightly.tar.gz')
+# tool = MModelTool('DTK','C++','None','ftp://ftp.mcs.anl.gov/pub/fathom/moab-nightly.tar.gz')
 tool = MModelTool(SurveyTools[3])
 print tool
 
+
 def cmd_exists(cmd):
-    return subprocess.call("type " + cmd, shell=True, 
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
+    return subprocess.call("type " + cmd, shell=True,
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
+
 
 def ExecCommand(shellCommand):
-    import sys, subprocess
     p = subprocess.Popen(shellCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
-    #print "[CMD: ", shellCommand, "]:\n", output
+    # print "[CMD: ", shellCommand, "]:\n", output
     return output.rstrip('\n')
 
+
 def ExecCommandStreaming(shellCommand):
-    import sys, subprocess
     p = subprocess.Popen(shellCommand, shell=True, stderr=subprocess.PIPE)
     while True:
         out = p.stderr.read(64)
-        if out == '' and p.poll() != None:
+        if out == '' and p.poll() is not None:
             break
         if out != '':
             sys.stdout.write(out)
             sys.stdout.flush()
 
+
 def GetGitRepo(toolname, giturl):
     command = "git clone " + giturl + " " + sandbox_dir + "/" + toolname
-    print '[',toolname,']','Cloning Git repository from URL:', giturl
+    print '[', toolname, ']', 'Cloning Git repository from URL:', giturl
     return ExecCommand(command)
+
 
 def GetSVNRepo(toolname, svnurl):
     command = "svn checkout " + svnurl + " " + sandbox_dir + "/" + toolname
-    print '[',toolname,']','Cloning Subversion repository from URL:', svnurl
+    print '[', toolname, ']', 'Cloning Subversion repository from URL:', svnurl
     return ExecCommand(command)
+
 
 def GetMercurialRepo(toolname, hgurl):
     command = "hg clone " + hgurl + " " + sandbox_dir + "/" + toolname
-    print '[',toolname,']','Cloning Mercurial repository from URL:', hgurl
+    print '[', toolname, ']', 'Cloning Mercurial repository from URL:', hgurl
     return ExecCommand(command)
+
 
 def GetTarball(toolname, url):
     filename = ExecCommand("basename " + url)
-    print '[',toolname,']','Downloading tarball from URL:', url
+    print '[', toolname, ']', 'Downloading tarball from URL:', url
     wgetcommand = ExecCommand("wget -O " + sandbox_dir + "/" + filename + " " + url)
     ExecCommand("mkdir -p " + sandbox_dir + "/" + toolname)
-    print '[',toolname,']','Deflating the tarball into the directory:', sandbox_dir + "/" + toolname
+    print '[', toolname, ']', 'Deflating the tarball into the directory:', sandbox_dir + "/" + toolname
     untarcommand = ExecCommand("tar -xf " + sandbox_dir + "/" + filename + " -C " + sandbox_dir + "/" + toolname + " --strip-components=1")
     return wgetcommand + "\n" + untarcommand
 
+
 def CloneRepo(toolname, vcs, url):
-    import os
-    if not os.path.exists(sandbox_dir+"/"+toolname):
+    if not os.path.exists(sandbox_dir + "/" + toolname):
         if vcs == 'Git':
             return GetGitRepo(toolname, url)
         elif vcs == 'Mercurial':
@@ -94,12 +103,12 @@ def CloneRepo(toolname, vcs, url):
         else:
             raise ValueError('[' + toolname + ']' + 'Unhandled VCS type. Please check the key: ' + vcs + ' and URL:' + url)
 
+
 def GetNSources(toolname):
-    import os
-    print '[',toolname,']','Computing source list (C, C++, Fortran, Python, Java)'
+    print '[', toolname, ']', 'Computing source list (C, C++, Fortran, Python, Java)'
     command_base = "find " + sandbox_dir + "/" + toolname + " "
     # count number of source files
-    command_end  = "| wc -l" # Use xargs wc -l, to count total lines in the sources
+    command_end = "| wc -l"  # Use xargs wc -l, to count total lines in the sources
     extension_dict = {}
     extension_dict['C'] = ['c', 'h']
     extension_dict['C++'] = ['cpp', 'hpp', 'hh', 'C', 'cc']
@@ -141,7 +150,7 @@ ExecCommandStreaming("scripts/get_metrixpp_logs " + tool.Name + " " + sandbox_di
 # 4) Launch static analyzer depending on languages supported
 
 # 4.a)       C/C++: cppcheck (cppcheck.sourceforge.net)
-if cmd_exists("cppcheck") and Sourcelist['C']+Sourcelist['C++'] > 0:
+if cmd_exists("cppcheck") and Sourcelist['C'] + Sourcelist['C++'] > 0:
     ExecCommandStreaming("scripts/get_cppcheck_logs " + tool.Name + " " + sandbox_dir + "/" + tool.Name)
 
 # 4.b)       Python: PyLint
@@ -158,4 +167,3 @@ if cmd_exists("radon") and Sourcelist['Python'] > 0:
 # 5) Aggregate static analyzer results
 #        a) For cppcheck, parse XML to find number of "errors"
 #        b) For PyLint, parse output to find "errors"
-
