23b36fde9ae305de3b0c39fac99b0bece2317be5
Sean Colby <seanmcolby@gmail.com>
2016-07-15 14:43:06 -0700

    integrated analysis pipline into methods, integrated these methods into a single method call, added a number of TODO items

--- main.py
+++ main.py
@@ -13,34 +13,7 @@ with open('data/mmodel-survey-tools.csv', 'rb') as csvfile:
 tool = mmst.MModelTool(surveyTools[3])
 print tool
 
-# For each of the tools, do the following actions
-
-# 1) First let us clone the repository
-cloneOpOut = tool.cloneRepo()
-
-# 2) Find out the number of sources and types
-sourceList = tool.getNSources()
-
-# 3) Run Metrix++ for C/C++/Java and PyLint for Python
-mmst.execCommandStreaming("scripts/get_metrixpp_logs " + tool.name + " " + mmst.sandbox_dir + "/" + tool.name)
-
-# 4) Launch static analyzer depending on languages supported
-
-# 4.a)       C/C++: cppcheck (cppcheck.sourceforge.net)
-if mmst.cmd_exists("cppcheck") and sourceList['C'] + sourceList['C++'] > 0:
-    mmst.execCommandStreaming("scripts/get_cppcheck_logs " + tool.Name + " " + mmst.sandbox_dir + "/" + tool.Name)
-
-# 4.b)       Python: PyLint
-if mmst.cmd_exists("pylint") and sourceList['Python'] > 0:
-    mmst.execCommand("mkdir -p " + mmst.sandbox_dir + "/pylint")
-    mmst.execCommandStreaming("find " + mmst.sandbox_dir + "/" + tool.name + "/ -name '*.py' | xargs pylint -E > " + mmst.sandbox_dir + "/pylint/" + tool.name + ".txt")
-
-# 4.c)       Python: Radon
-if mmst.cmd_exists("radon") and sourceList['Python'] > 0:
-    mmst.execCommand("mkdir -p " + mmst.sandbox_dir + "/radon")
-    mmst.execCommandStreaming("radon cc -a --total-average -s " + mmst.sandbox_dir + "/" + tool.name + " > " + mmst.sandbox_dir + "/radon/" + tool.name + ".txt")
-
-
+result = tool.analyze()
 # 5) Aggregate static analyzer results
 #        a) For cppcheck, parse XML to find number of "errors"
 #        b) For PyLint, parse output to find "errors"
--- mmsofttools.py
+++ mmsofttools.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 import os
 
-sandbox_dir = 'sandbox'
+sandbox_dir = './sandbox'
 subprocess.call(["mkdir", "-p", sandbox_dir])
 
 
@@ -12,6 +12,11 @@ class MModelTool:
         self.languages = toolDict['Languages']
         self.vcs = toolDict['VCS']
         self.repository = toolDict['Repository']
+
+        self.c = False
+        self.python = False
+        self.java = False
+
 
     def __str__(self):
         return "[ " + self.name + " ] |VCS|: " + self.vcs + ", |Supported Languages|: " + self.languages + ", |Repository|: " + self.repository
@@ -29,23 +34,28 @@ class MModelTool:
             else:
                 raise ValueError('[' + self.name + ']' + 'Unhandled VCS type. Please check the key: ' + self.vcs + ' and URL:' + self.repository)
 
-    def getNSources(self):
+    def checkSource(self):
         print '[', self.name, ']', 'Computing source list (C, C++, Fortran, Python, Java)'
         command_base = "find " + sandbox_dir + "/" + self.name + " "
         # count number of source files
         command_end = "| wc -l"  # Use xargs wc -l, to count total lines in the sources
+
         extension_dict = {}
         extension_dict['C'] = ['c', 'h']
         extension_dict['C++'] = ['cpp', 'hpp', 'hh', 'C', 'cc']
         extension_dict['Fortran'] = ['h90', 'f90', 'F90', 'f', 'F']
         extension_dict['Python'] = ['py']
         extension_dict['Java'] = ['java']
+
+        # are the following lines needed?
         cppfiles = "-name '*.cpp' -o -name '*.hpp' -o -name '*.hh' -o -name '*.C' -o -name '*.cc'"
         cfiles = "-name '*.c' -o -name '*.h'"
         fortfiles = "-name '*.h90' -o -name '*.f90' -o -name '*.F90' -o -name '*.f' -o -name '*.F'"
         pyfiles = "-name '*.py'"
         javafiles = "-name '*.java'"
-        source_count = {}
+        #
+
+        sourceCount = {}
         for key, value in extension_dict.iteritems():
             srcexts = value
             command = command_base
@@ -56,14 +66,89 @@ class MModelTool:
                 command = command + "-name '*." + srcext + "' "
                 index = index + 1
             command = command + command_end
-            source_count[key] = int(execCommand(command))
-        print source_count
-        return source_count
+            sourceCount[key] = int(execCommand(command))
+
+        # store booleans for each code type if source files present
+        if sourceCount['C'] + sourceCount['C++'] > 0:
+            self.c = True
+        if sourceCount['Python'] > 0:
+            self.python = True
+        if sourceCount['Java'] > 0:
+            self.java = True
+        if sourceCount['Fortran'] > 0:
+            self.fortran = True
+
+        print sourceCount
+        return sourceCount
+
+    def analyzeJavaC(self):
+        # run metrix++
+        # runs regardless of source contents right now.  do we want to check for Java/C/C++?
+        execCommandStreaming("./scripts/get_metrixpp_logs " + self.name + " " + sandbox_dir + "/" + self.name)
+
+        # run cppcheck in installed and source contains C/C++ code
+        if cmd_exists("cppcheck") and self.c is True:
+            execCommandStreaming("scripts/get_cppcheck_logs " + self.name + " " + sandbox_dir + "/" + self.name)
+
+        # TODO: parse output and return stats
+
+    def analyzePython(self):
+        # run pylint if installed and source contains Python code
+        if cmd_exists("pylint") and self.python is True:
+            execCommand("mkdir -p " + sandbox_dir + "/pylint")
+            execCommandStreaming("find " + sandbox_dir + "/" + self.name + "/ -name '*.py' | xargs pylint -E > " + sandbox_dir + "/pylint/" + self.name + ".txt")
+
+        # run radon if installed and source contains Python code
+        if cmd_exists("radon") and self.python is True:
+            execCommand("mkdir -p " + sandbox_dir + "/radon")
+            execCommandStreaming("radon cc -a --total-average -s " + sandbox_dir + "/" + self.name + " > " + sandbox_dir + "/radon/" + self.name + ".txt")
+
+        # TODO: parse output and return stats
+
+    def analyzeFortran(self):
+        # TODO: add commands used for assessing Fortran source
+        # TODO: parse output and return stats
+        pass
+
+    def analyze(self):
+        result = Result()
+
+        # 1) First let us clone the repository
+        result.repoOutput = self.cloneRepo()
+
+        # 2) Find out the number of sources and types
+        result.sourceList = self.checkSource()
+
+        # 3) Run Metrix++ for C/C++/Java and cppcheck for C/C++
+        self.analyzeJavaC()
+
+        # 4) Run PyLint and Radon for Python
+        self.analyzePython()
+
+        # TODO: 5) Run * for Fortran
+        # self.analyzeFortran()
+
+        # TODO: 5) Aggregate static analyzer results
+        # TODO: a) For cppcheck, parse XML to find number of "errors"
+        # TODO: b) For PyLint, parse output to find "errors"
+
+        return result
+
+
+class Result:
+    '''A container object to store relevant results from the tool analysis
+            Returns
+            -------
+            self.sourceCount : ...
+            self.repoOutput : ...
+    '''
+
+    def __init__(self):
+        pass
 
 
 def cmd_exists(cmd):
-    return subprocess.call("type " + cmd, shell=True,
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
+    return subprocess.call("type " + cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
 
 
 def execCommand(shellCommand):
