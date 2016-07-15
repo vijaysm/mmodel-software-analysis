83f25e24837c4a1acc0bd9cca7757c53741633a6
Sean Colby <seanmcolby@gmail.com>
2016-07-15 11:33:51 -0700

    made cloneRepo and getNSources methods of the MModelTool class.  Enforced camelCase formatting for non-class variables and functions.

--- main.py
+++ main.py
@@ -1,45 +1,44 @@
-from mmsofttools import *
+import mmsofttools as mmst
+import csv
 
-quit()
-SurveyTools = []
+surveyTools = []
 with open('data/mmodel-survey-tools.csv', 'rb') as csvfile:
     rowreader = csv.DictReader(csvfile)
     for row in rowreader:
-        SurveyTools.append(row)
-    # print SurveyTools
+        surveyTools.append(row)
 
 # Replace with a recursive parse of the CSV file to get the tool list
 # tool = MModelTool('DTK','C++','Git','https://github.com/ORNL-CEES/DataTransferKit.git')
 # tool = MModelTool('DTK','C++','None','ftp://ftp.mcs.anl.gov/pub/fathom/moab-nightly.tar.gz')
-tool = MModelTool(SurveyTools[3])
+tool = mmst.MModelTool(surveyTools[3])
 print tool
 
 # For each of the tools, do the following actions
 
 # 1) First let us clone the repository
-CloneOpOut = CloneRepo(tool.Name, tool.VCS, tool.Repository)
+cloneOpOut = tool.cloneRepo()
 
 # 2) Find out the number of sources and types
-Sourcelist = GetNSources(tool.Name)
+sourceList = tool.getNSources()
 
 # 3) Run Metrix++ for C/C++/Java and PyLint for Python
-ExecCommandStreaming("scripts/get_metrixpp_logs " + tool.Name + " " + sandbox_dir + "/" + tool.Name)
+mmst.execCommandStreaming("scripts/get_metrixpp_logs " + tool.name + " " + mmst.sandbox_dir + "/" + tool.name)
 
 # 4) Launch static analyzer depending on languages supported
 
 # 4.a)       C/C++: cppcheck (cppcheck.sourceforge.net)
-if cmd_exists("cppcheck") and Sourcelist['C'] + Sourcelist['C++'] > 0:
-    ExecCommandStreaming("scripts/get_cppcheck_logs " + tool.Name + " " + sandbox_dir + "/" + tool.Name)
+if mmst.cmd_exists("cppcheck") and sourceList['C'] + sourceList['C++'] > 0:
+    mmst.execCommandStreaming("scripts/get_cppcheck_logs " + tool.Name + " " + mmst.sandbox_dir + "/" + tool.Name)
 
 # 4.b)       Python: PyLint
-if cmd_exists("pylint") and Sourcelist['Python'] > 0:
-    ExecCommand("mkdir -p " + sandbox_dir + "/pylint")
-    ExecCommandStreaming("find " + sandbox_dir + "/" + tool.Name + "/ -name '*.py' | xargs pylint -E > " + sandbox_dir + "/pylint/" + tool.Name + ".txt")
+if mmst.cmd_exists("pylint") and sourceList['Python'] > 0:
+    mmst.execCommand("mkdir -p " + mmst.sandbox_dir + "/pylint")
+    mmst.execCommandStreaming("find " + mmst.sandbox_dir + "/" + tool.name + "/ -name '*.py' | xargs pylint -E > " + mmst.sandbox_dir + "/pylint/" + tool.name + ".txt")
 
 # 4.c)       Python: Radon
-if cmd_exists("radon") and Sourcelist['Python'] > 0:
-    ExecCommand("mkdir -p " + sandbox_dir + "/radon")
-    ExecCommandStreaming("radon cc -a --total-average -s " + sandbox_dir + "/" + tool.Name + " > " + sandbox_dir + "/radon/" + tool.Name + ".txt")
+if mmst.cmd_exists("radon") and sourceList['Python'] > 0:
+    mmst.execCommand("mkdir -p " + mmst.sandbox_dir + "/radon")
+    mmst.execCommandStreaming("radon cc -a --total-average -s " + mmst.sandbox_dir + "/" + tool.name + " > " + mmst.sandbox_dir + "/radon/" + tool.name + ".txt")
 
 
 # 5) Aggregate static analyzer results
--- mmsofttools.py
+++ mmsofttools.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python
-
-import csv
 import subprocess
 import sys
 import os
@@ -10,20 +7,58 @@ subprocess.call(["mkdir", "-p", sandbox_dir])
 
 
 class MModelTool:
-    def __init__(self, Tool, langs, vcs, url):
-        self.Name = Tool
-        self.Languages = langs
-        self.VCS = vcs
-        self.Repository = url
-
-    def __init__(self, ToolDict):
-        self.Name = ToolDict['Name']
-        self.Languages = ToolDict['Languages']
-        self.VCS = ToolDict['VCS']
-        self.Repository = ToolDict['Repository']
+    def __init__(self, toolDict):
+        self.name = toolDict['Name']
+        self.languages = toolDict['Languages']
+        self.vcs = toolDict['VCS']
+        self.repository = toolDict['Repository']
 
     def __str__(self):
-        return "[ " + self.Name + " ] |VCS|: " + self.VCS + ", |Supported Languages|: " + self.Languages + ", |Repository|: " + self.Repository
+        return "[ " + self.name + " ] |VCS|: " + self.vcs + ", |Supported Languages|: " + self.languages + ", |Repository|: " + self.repository
+
+    def cloneRepo(self):
+        if not os.path.exists(sandbox_dir + "/" + self.name):
+            if self.vcs == 'Git':
+                return getGitRepo(self.name, self.repository)
+            elif self.vcs == 'Mercurial':
+                return getMercurialRepo(self.name, self.repository)
+            elif self.vcs == 'Subversion':
+                return getSVNRepo(self.name, self.repository)
+            elif self.vcs == 'None':
+                return getTarball(self.name, self.repository)
+            else:
+                raise ValueError('[' + self.name + ']' + 'Unhandled VCS type. Please check the key: ' + self.vcs + ' and URL:' + self.repository)
+
+    def getNSources(self):
+        print '[', self.name, ']', 'Computing source list (C, C++, Fortran, Python, Java)'
+        command_base = "find " + sandbox_dir + "/" + self.name + " "
+        # count number of source files
+        command_end = "| wc -l"  # Use xargs wc -l, to count total lines in the sources
+        extension_dict = {}
+        extension_dict['C'] = ['c', 'h']
+        extension_dict['C++'] = ['cpp', 'hpp', 'hh', 'C', 'cc']
+        extension_dict['Fortran'] = ['h90', 'f90', 'F90', 'f', 'F']
+        extension_dict['Python'] = ['py']
+        extension_dict['Java'] = ['java']
+        cppfiles = "-name '*.cpp' -o -name '*.hpp' -o -name '*.hh' -o -name '*.C' -o -name '*.cc'"
+        cfiles = "-name '*.c' -o -name '*.h'"
+        fortfiles = "-name '*.h90' -o -name '*.f90' -o -name '*.F90' -o -name '*.f' -o -name '*.F'"
+        pyfiles = "-name '*.py'"
+        javafiles = "-name '*.java'"
+        source_count = {}
+        for key, value in extension_dict.iteritems():
+            srcexts = value
+            command = command_base
+            index = 0
+            for srcext in srcexts:
+                if index:
+                    command = command + " -o "
+                command = command + "-name '*." + srcext + "' "
+                index = index + 1
+            command = command + command_end
+            source_count[key] = int(execCommand(command))
+        print source_count
+        return source_count
 
 
 def cmd_exists(cmd):
@@ -31,14 +66,14 @@ def cmd_exists(cmd):
                            stdout=subprocess.PIPE, stderr=subprocess.PIPE) == 0
 
 
-def ExecCommand(shellCommand):
+def execCommand(shellCommand):
     p = subprocess.Popen(shellCommand, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     (output, err) = p.communicate()
     # print "[CMD: ", shellCommand, "]:\n", output
     return output.rstrip('\n')
 
 
-def ExecCommandStreaming(shellCommand):
+def execCommandStreaming(shellCommand):
     p = subprocess.Popen(shellCommand, shell=True, stderr=subprocess.PIPE)
     while True:
         out = p.stderr.read(64)
@@ -49,75 +84,29 @@ def ExecCommandStreaming(shellCommand):
             sys.stdout.flush()
 
 
-def GetGitRepo(toolname, giturl):
+def getGitRepo(toolname, giturl):
     command = "git clone " + giturl + " " + sandbox_dir + "/" + toolname
     print '[', toolname, ']', 'Cloning Git repository from URL:', giturl
-    return ExecCommand(command)
+    return execCommand(command)
 
 
-def GetSVNRepo(toolname, svnurl):
+def getSVNRepo(toolname, svnurl):
     command = "svn checkout " + svnurl + " " + sandbox_dir + "/" + toolname
     print '[', toolname, ']', 'Cloning Subversion repository from URL:', svnurl
-    return ExecCommand(command)
+    return execCommand(command)
 
 
-def GetMercurialRepo(toolname, hgurl):
+def getMercurialRepo(toolname, hgurl):
     command = "hg clone " + hgurl + " " + sandbox_dir + "/" + toolname
     print '[', toolname, ']', 'Cloning Mercurial repository from URL:', hgurl
-    return ExecCommand(command)
+    return execCommand(command)
 
 
-def GetTarball(toolname, url):
-    filename = ExecCommand("basename " + url)
+def getTarball(toolname, url):
+    filename = execCommand("basename " + url)
     print '[', toolname, ']', 'Downloading tarball from URL:', url
-    wgetcommand = ExecCommand("wget -O " + sandbox_dir + "/" + filename + " " + url)
-    ExecCommand("mkdir -p " + sandbox_dir + "/" + toolname)
+    wgetcommand = execCommand("wget -O " + sandbox_dir + "/" + filename + " " + url)
+    execCommand("mkdir -p " + sandbox_dir + "/" + toolname)
     print '[', toolname, ']', 'Deflating the tarball into the directory:', sandbox_dir + "/" + toolname
-    untarcommand = ExecCommand("tar -xf " + sandbox_dir + "/" + filename + " -C " + sandbox_dir + "/" + toolname + " --strip-components=1")
+    untarcommand = execCommand("tar -xf " + sandbox_dir + "/" + filename + " -C " + sandbox_dir + "/" + toolname + " --strip-components=1")
     return wgetcommand + "\n" + untarcommand
-
-
-def CloneRepo(toolname, vcs, url):
-    if not os.path.exists(sandbox_dir + "/" + toolname):
-        if vcs == 'Git':
-            return GetGitRepo(toolname, url)
-        elif vcs == 'Mercurial':
-            return GetMercurialRepo(toolname, url)
-        elif vcs == 'Subversion':
-            return GetSVNRepo(toolname, url)
-        elif vcs == 'None':
-            return GetTarball(toolname, url)
-        else:
-            raise ValueError('[' + toolname + ']' + 'Unhandled VCS type. Please check the key: ' + vcs + ' and URL:' + url)
-
-
-def GetNSources(toolname):
-    print '[', toolname, ']', 'Computing source list (C, C++, Fortran, Python, Java)'
-    command_base = "find " + sandbox_dir + "/" + toolname + " "
-    # count number of source files
-    command_end = "| wc -l"  # Use xargs wc -l, to count total lines in the sources
-    extension_dict = {}
-    extension_dict['C'] = ['c', 'h']
-    extension_dict['C++'] = ['cpp', 'hpp', 'hh', 'C', 'cc']
-    extension_dict['Fortran'] = ['h90', 'f90', 'F90', 'f', 'F']
-    extension_dict['Python'] = ['py']
-    extension_dict['Java'] = ['java']
-    cppfiles = "-name '*.cpp' -o -name '*.hpp' -o -name '*.hh' -o -name '*.C' -o -name '*.cc'"
-    cfiles = "-name '*.c' -o -name '*.h'"
-    fortfiles = "-name '*.h90' -o -name '*.f90' -o -name '*.F90' -o -name '*.f' -o -name '*.F'"
-    pyfiles = "-name '*.py'"
-    javafiles = "-name '*.java'"
-    source_count = {}
-    for key, value in extension_dict.iteritems():
-        srcexts = value
-        command = command_base
-        index = 0
-        for srcext in srcexts:
-            if index:
-                command = command + " -o "
-            command = command + "-name '*." + srcext + "' "
-            index = index + 1
-        command = command + command_end
-        source_count[key] = int(ExecCommand(command))
-    print source_count
-    return source_count
