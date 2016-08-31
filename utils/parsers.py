4bdcf2b5b5b2d350b103d31233d9917dc8c60e6c
Sean Colby <seanmcolby@gmail.com>
2016-08-31 11:15:36 -0700

    parsing pylint tables.  need to consolidate information

--- utils/parsers.py
+++ utils/parsers.py
@@ -52,16 +52,93 @@ class CPPcheck():
 
 
 class PyLint():
-    def __init__(self, srcDir):
+    def __init__(self):
+        pass
+
+    def parse(self, logDir):
+        d = {}
         self.files = []
-        for dirpath, dirnames, filenames in os.walk(srcDir):
-            for filename in [f for f in filenames if f.endswith(".py")]:
+        for dirpath, dirnames, filenames in os.walk(logDir):
+            for filename in [f for f in filenames if f.endswith(".txt")]:
                 self.files.append(os.path.join(dirpath, filename))
 
-    def parse(self):
+        for fn in self.files:
+            key = os.path.basename(fn)
+            d[key] = {}
+            d[key]['errors'] = []
+            d[key]['duplication'] = []
+            d[key]['stats'] = []
+            d[key]['messagesByCategory'] = []
+            d[key]['messages'] = []
+            d[key]['global'] = []
+            tableKey = None
+            with open(fn, 'r') as f:
+                for l in f:
+                    if l is None or '':
+                        pass
+                    elif l.startswith(' ') or l.startswith('\n'):
+                        pass
+                    elif re.match(r"[A-Z]: ", l) is not None:
+                        d[key]['errors'].append(l.strip())
+                    elif 'Duplication' in l:
+                        tableKey = 'duplication'
+                    elif 'Statistics by type' in l:
+                        tableKey = 'stats'
+                    elif 'Messages by category' in l:
+                        tableKey = 'messagesByCategory'
+                    elif l.strip() == 'Messages':
+                        tableKey = 'messages'
+                    elif 'Global evaluation' in l:
+                        tableKey = 'global'
+                    elif tableKey is not None and re.search(r"[a-z]", l) is not None:
+                        d[key][tableKey].append(l.strip())
+
+
+    def parse_debug(self, fn):
+        d = {}
+        tableKey = None
         with open(fn, 'r') as f:
+            key = os.path.basename(fn)
+            d[key] = {}
+            d[key]['errors'] = []
+            d[key]['duplication'] = []
+            d[key]['stats'] = []
+            d[key]['messagesByCategory'] = []
+            d[key]['messages'] = []
+            d[key]['global'] = []
+            d[key]['raw'] = []
             for l in f:
-                print l
+                if l is None or '':
+                    pass
+                elif l.startswith(' ') or l.startswith('\n'):
+                    pass
+                elif re.match(r"[A-Z]: ", l) is not None:
+                    d[key]['errors'].append(l.strip())
+                elif 'Duplication' in l:
+                    tableKey = 'duplication'
+                elif 'Statistics by type' in l:
+                    tableKey = 'stats'
+                elif 'Messages by category' in l:
+                    tableKey = 'messagesByCategory'
+                elif l.strip() == 'Messages':
+                    tableKey = 'messages'
+                elif 'Global evaluation' in l:
+                    tableKey = 'global'
+                elif 'Raw metrics' in l:
+                    tableKey = 'raw'
+                elif tableKey is not None and re.search(r"[a-z]", l) is not None:
+                    d[key][tableKey].append(l.strip())
+
+        for table in d[key].keys():
+            if table is not 'errors' and table is not 'global':
+                colnames = [s.strip() for s in d[key][table][0].split('|')][1:-1]
+                print
+                print colnames
+                for i in range(1, len(d[key][table])):
+                    row = [s.strip() for s in d[key][table][i].split('|')][1:-1]
+                    name = row[0]
+                    values = row[1:]
+                    print name, values
 
 
 class Radon():
@@ -100,4 +177,4 @@ class Radon():
 # test/debug
 if __name__ == '__main__':
     pl = PyLint()
-    pl.parse('/home/sean/Desktop/temp.txt')
+    pl.parse_debug('../sandbox/pylint/Amuse/CDG_gd.txt')
