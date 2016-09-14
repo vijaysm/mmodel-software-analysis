2fc434d94ac7f24be35f5972e329fe2dc835d329
Sean Colby <seanmcolby@gmail.com>
2016-09-14 10:24:07 -0700

    removed print statements, added JSON output

--- dict_reader.py
+++ dict_reader.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 
 
 # path to saved dictionary
@@ -7,34 +8,8 @@ fp = './mmodel_dictionary.npy'
 # load the dictionary
 d = np.load(fp).item()
 
-# first level
-print 'Top level is accessed by name of each tool: d.keys() ='
-print d.keys()
-print
+d_amuse = d['Amuse']
+j = json.dumps(d_amuse)
 
-# second level
-print 'The next level is for each analysis tool (radon, pylint, metrix):'
-print "e.g., d['DTK'].keys() =", d['DTK'].keys()
-print 'NOTE: an entry will exist only if that tool was run:'
-print "\tOpenMI has nothing (for some reason...): d['OpenMI'] =", d['OpenMI']
-print "\tAiiDA only has radon: d['AiiDA'].keys() =", d['AiiDA'].keys()
-print
-
-# third level
-print 'Each analysis tool has its own structure from here on out:'
-print "\tRadon: has a key for each .py file and a total, e.g. d['DTK']['radon'].keys() =", d['DTK']['radon'].keys()
-print "\t'total' contains d['DTK']['radon']['total'].keys() =", d['DTK']['radon']['total'].keys()
-print "\tEach of the above keys should produce a value."
-print
-print "\tPyLint:  has a key for every .py file, e.g. d['DTK']['pylint'].keys() =", d['DTK']['pylint'].keys()
-print "\t'conf.py' contains d['DTK']['pylint']['conf.py'].keys() =", d['DTK']['pylint']['conf.py'].keys()
-print "\tPyLint is the trickiest as each of the above keys corresponds to a table, which has its own keys..."
-print "\tI recommend just playing around a bit, e.g.:"
-print "\t\td['DTK']['pylint']['conf.py']['regions'].keys() =", d['DTK']['pylint']['conf.py']['regions'].keys()
-print "\t\td['DTK']['pylint']['conf.py']['regions']['function'].keys() =", d['DTK']['pylint']['conf.py']['regions']['function'].keys()
-print "\t\td['DTK']['pylint']['conf.py']['regions']['function']['number'] =", d['DTK']['pylint']['conf.py']['regions']['function']['number']
-print "\tWe had to go pretty deep to get that 0, but you get the idea."
-print
-print "\tMetrixPP: has a key for each parameter d['DTK']['metrix'].keys() ="
-print '\t', d['DTK']['metrix'].keys()
-print '\tEach of the above keys should contain values for "average" and "total".'
+with open('data.txt', 'w') as outfile:
+    json.dump(d_amuse, outfile)
