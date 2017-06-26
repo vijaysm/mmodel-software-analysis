f6d3e43a470a6e8b28ac314de2e232f13ea02800
Sean Colby <seanmcolby@gmail.com>
2017-06-26 12:24:56 -0700

    parsing and plotting functionality

--- dict_reader.py
+++ dict_reader.py
@@ -1,5 +1,7 @@
 import numpy as np
-import json
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 
 # path to saved dictionary
@@ -8,8 +10,81 @@ fp = './mmodel_dictionary.npy'
 # load the dictionary
 d = np.load(fp).item()
 
-d_amuse = d['Amuse']
-j = json.dumps(d_amuse)
+final = {'tool': [], 'lines': [], 'comments': [], 'errors': [], 'docs': [], 'size': []}
+for t in d.keys():
+    comments = 0
+    lines = 0
+    size = 0
+    errors = 0
+    docs = 0
+    for a in d[t]:
+        if a == 'metrix' and d[t][a] is not None:
+            comments += d[t][a]['std.code.lines:comments']['total']
+            lines += d[t][a]['std.code.lines:code']['total']
+            size += d[t][a]['std.general:size']['total']
 
-with open('data.txt', 'w') as outfile:
-    json.dump(d_amuse, outfile)
+        elif a == 'radon' and d[t][a] is not None:
+            try:
+                comments += d[t][a]['total']['comments']
+                lines += d[t][a]['total']['loc']
+            except:
+                pass
+
+        elif a == 'pylint' and d[t][a] is not None:
+            llines = 0
+            lcomments = 0
+            lsize = 0
+            ldocs = 0
+            nerrors = 0
+            for f in d[t][a].keys():
+                try:
+                    # print(d[t][a][f]['messages'].keys())
+                    llines += d[t][a][f]['metrics']['code']['number']
+                    lcomments += d[t][a][f]['metrics']['comment']['number']
+                    ldocs += d[t][a][f]['metrics']['docstring']['number']
+                    nerrors += len(d[t][a][f]['errors'])
+                except:
+                    pass
+
+            comments += lcomments
+            lines += llines
+            errors += nerrors
+            docs += ldocs
+
+
+    final['tool'].append(t)
+    final['lines'].append(lines)
+    final['comments'].append(comments)
+    final['errors'].append(errors)
+    final['docs'].append(docs)
+    final['size'].append(size)
+
+final = pd.DataFrame(final)
+final['cpl'] = final['comments'] / final['lines']
+final['epl'] = final['errors'] / final['lines']
+final['dpl'] = final['docs'] / final['lines']
+
+ax = sns.barplot(x='tool', y='size', data=final)
+plt.xticks(rotation=90)
+plt.ylabel('Size (bytes)')
+plt.show()
+
+ax = sns.barplot(x='tool', y='lines', data=final)
+plt.xticks(rotation=90)
+plt.ylabel('LOC')
+plt.show()
+
+ax = sns.barplot(x='tool', y='cpl', data=final)
+plt.xticks(rotation=90)
+plt.ylabel('Comments per LOC')
+plt.show()
+
+ax = sns.barplot(x='tool', y='epl', data=final)
+plt.xticks(rotation=90)
+plt.ylabel('Errors per LOC')
+plt.show()
+
+ax = sns.barplot(x='tool', y='dpl', data=final)
+plt.xticks(rotation=90)
+plt.ylabel('Docstrings per LOC')
+plt.show()
