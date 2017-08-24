5b63b3a54fde03b1cec2d9aa9b4b7f0a2d48b693
Sean Colby <seanmcolby@gmail.com>
2017-08-24 12:29:43 -0700

    enable all plots

--- dict_reader.py
+++ dict_reader.py
@@ -18,7 +18,6 @@ pylint = []
 metrix = []
 radon = []
 for t in data.keys():
-    print(t)
     languages = [x.lower() for x in tools.loc[tools['Name'] == t]['Languages'].values[0].split('/')]
 
     for a in data[t]:
@@ -29,6 +28,7 @@ for t in data.keys():
                 d['Comments'] = data[t][a]['std.code.lines:comments']['total']
                 d['Code'] = data[t][a]['std.code.lines:code']['total']
                 d['Complexity'] = data[t][a]['std.code.complexity:cyclomatic']['total']
+                # d['Errors'] = data[t][a]['std.general.procerrors']   
                 metrix.append(d)
 
         if 'python' in languages:
@@ -56,10 +56,9 @@ for t in data.keys():
                 pylint.append(d)
             elif a == 'radon' and bool(data[t][a]):
                 d = {}
-                d['tool'] = t
-                d['Complexity'] = data[t][a].keys()
-                print(d['Complexity'])
-                quit()
+                d['Tool'] = t
+                d['Complexity'] = data[t][a]['Complexity']
+                radon.append(d)
 
 # pylint prep
 pylint = pd.DataFrame(pylint)
@@ -74,38 +73,43 @@ metrix.sort_values(by='Tool', inplace=True)
 metrix.index = metrix['Tool']
 metrix['cpl'] = metrix['Comments'] / metrix['Code']
 
+# radon prep
+radon = pd.DataFrame(radon)
+radon.sort_values(by='Tool', inplace=True)
+radon.index = radon['Tool']
+
 # # stacked plots
-# stacked_bar_chart(pylint, ['Code', 'Comments', 'Docstrings'], 'Tool', 'Lines', 'stacked.png', Color('lightgray'), Color('gray'))
-# stacked_bar_chart(metrix, ['Code', 'Comments'], 'Tool', 'Lines', 'stacked2.png', Color('lightgray'), Color('gray'))
+stacked_bar_chart(pylint, ['Code', 'Comments', 'Docstrings'], 'Tool', 'Lines', 'figs/python_stacked.png', Color('lightgray'), Color('gray'))
+stacked_bar_chart(metrix, ['Code', 'Comments'], 'Tool', 'Lines', 'figs/cpp_stacked.png', Color('lightgray'), Color('gray'))
 
-# # comments per line code
-# ax = sns.barplot(x='Tool', y='cpl', data=pylint, color='gray')
-# plt.xticks(rotation=90)
-# ax.set(xlabel='', ylabel='Comments per LOC')
-# ax.tick_params(bottom='off')
-# plt.tight_layout()
+# comments per line code
+ax = sns.barplot(x='Tool', y='cpl', data=pylint, color='gray')
+plt.xticks(rotation=90)
+ax.set(xlabel='', ylabel='Comments per LOC')
+ax.tick_params(bottom='off')
+plt.tight_layout()
 # plt.show()
-# # plt.savefig('cploc.png')
-# # plt.close()
+plt.savefig('figs/python_cploc.png')
+plt.close()
 
-# ax = sns.barplot(x='Tool', y='cpl', data=metrix, color='gray')
-# plt.xticks(rotation=90)
-# ax.set(xlabel='', ylabel='Comments per LOC')
-# ax.tick_params(bottom='off')
-# plt.tight_layout()
+ax = sns.barplot(x='Tool', y='cpl', data=metrix, color='gray')
+plt.xticks(rotation=90)
+ax.set(xlabel='', ylabel='Comments per LOC')
+ax.tick_params(bottom='off')
+plt.tight_layout()
 # plt.show()
-# # plt.savefig('cploc.png')
-# # plt.close()
+plt.savefig('figs/cpp_cploc.png')
+plt.close()
 
-# # errors per line code
-# ax = sns.barplot(x='Tool', y='epl', data=pylint, color='gray')
-# plt.xticks(rotation=90)
-# ax.set(xlabel='', ylabel='Errors per LOC')
-# ax.tick_params(bottom='off')
-# plt.tight_layout()
+# errors per line code
+ax = sns.barplot(x='Tool', y='epl', data=pylint, color='gray')
+plt.xticks(rotation=90)
+ax.set(xlabel='', ylabel='Errors per LOC')
+ax.tick_params(bottom='off')
+plt.tight_layout()
 # plt.show()
-# # plt.savefig('eplc.png')
-# # plt.close()
+plt.savefig('figs/python_eplc.png')
+plt.close()
 
 # cyclomatic complexity
 ax = sns.barplot(x='Tool', y='Complexity', data=metrix, color='gray')
@@ -114,4 +118,14 @@ ax.set(xlabel='', ylabel='Cyclomatic Complexity')
 ax.tick_params(bottom='off')
 ax.set_yscale("log")
 plt.tight_layout()
-plt.show()
+plt.savefig('figs/cpp_cc.png')
+plt.close()
+
+ax = sns.barplot(x='Tool', y='Complexity', data=radon, color='gray')
+plt.xticks(rotation=90)
+ax.set(xlabel='', ylabel='Cyclomatic Complexity')
+ax.tick_params(bottom='off')
+ax.set_yscale("log")
+plt.tight_layout()
+plt.savefig('figs/python_cc.png')
+plt.close()
